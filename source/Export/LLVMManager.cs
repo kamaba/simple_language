0f1b8a55fbac62f8698c8b3c6914d884d1f95c55
kamaba233 <kamaba233@gmail.com>
2025-07-12 18:56:29 +0800

    1

---
+++ source/Export/LLVMManager.cs
@@ -0,0 +1,29 @@
+﻿//****************************************************************************
+//  File:      LLVMManager.cs
+// ------------------------------------------------
+//  Copyright (c) kamaba233@gmail.com
+//  DateTime: 2022/11/30 12:00:00
+//  Description:  manager Export other lanuage or il etc.
+//****************************************************************************
+
+using SimpleLanguage.Project;
+using System.IO;
+using Swigged.LLVM;
+using System.Reflection;
+using System;
+
+namespace SimpleLanguage.ExportLanguage
+{
+    public class LLVMManager
+    {
+
+        public void CreateModule()
+        {
+            LLVM.InitializeAllTargets();
+            LLVM.InitializeAllTargetMCs();
+            LLVM.InitializeAllTargetInfos();
+            LLVM.InitializeAllAsmPrinters();
+
+        }
+    }
+}
