8b49e001bdd1558cdafa4703a6106e4a34c47fdf
kamaba233 <kamaba233@gmail.com>
2025-10-24 19:32:48 +0800

    111111

--- source/Compile/FileMeta/FileMetaMemberFunction.cs
+++ source/Compile/FileMeta/FileMetaMemberFunction.cs
@@ -1,5 +1,5 @@
 ﻿//****************************************************************************
-//  File:      FileMetaMemberMethod.cs
+//  File:      FileMetaMemberFunction.cs
 // ------------------------------------------------
 //  Copyright (c) kamaba233@gmail.com
 //  DateTime: 2022/5/12 12:00:00
